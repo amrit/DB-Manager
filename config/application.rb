19ddafca51654cd77285d3da647dbde6fc084551
Amrit Ayalur <a.ayalur@gmail.com>
2012-06-18 14:12:59 -0500

    bugga

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require 'rails/all'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  Bundler.require(:default, :assets, Rails.env)
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
@@ -56,7 +56,11 @@ module Mobcapdb
     # Enable the asset pipeline
     config.assets.enabled = true
     
-    config.assets.precompile += %w(active_admin.css active_admin.js)
+    #Added to fix devise/active admin issue ?
+    config.assets.initialize_on_precompile = false
+
+    # Precompile additional assets. Defaults to [application.js, application.css, non-JS/CSS]
+    config.assets.precompile += ['active_admin.css.scss', 'active_admin.js']
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
